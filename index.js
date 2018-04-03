0079f7b97be32efc37949b15b733b1333ccb183d
tomoversluizen <tom.oversluizen@hotmail.com>
2018-04-03 20:55:04 +0200

    added ejs package + renaming + error page

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ express()
   .set('view engine', 'ejs')
   .set('views', 'views')
   .use(express.static('static'))
+  .get('/', home)
+  .get('/', detail)
+  .use(notFound)
   .listen(1907)
 
 console.log('Port 1907 available')
+
+function home(req, res){
+  res.render('list.ejs')
+}
+
+function detail(req, res){
+  res.render('index.ejs')
+}
+
+function notFound(req, res) {
+  res.status(404).render('error.ejs')
+}
